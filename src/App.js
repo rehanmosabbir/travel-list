5d9232f152947faa40bad4176392b5a62763cd33
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-09 23:50:11 +0600

    add index.css and Logo,Form,PackingList and Stats component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export default function App() {}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return <div className="add-form"></div>;
+}
+function PackingList() {}
+function Stats() {}
